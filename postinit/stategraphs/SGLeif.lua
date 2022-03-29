da29081ffe201dcb30fab4e492a5a335d41b842f
Scrimbles <40772985+Shodance@users.noreply.github.com>
2022-03-29 13:40:12 -0400

    Treeguards now reset their attack range when exiting any state that changes it, ported the treeguard changes to the better way of wrapping stategraph events, removed their hit stun immunity (klei 'fixed' it with wanda stuff)

--- postinit/stategraphs/SGLeif.lua
+++ postinit/stategraphs/SGLeif.lua
@@ -3,6 +3,25 @@ GLOBAL.setfenv(1, GLOBAL)
 
 env.AddStategraphPostInit("leif", function(inst)
 
+
+local _OldAttackEvent = inst.events["doattack"].fn
+	inst.events["doattack"].fn = function(inst, data)
+		if not inst.components.health:IsDead() and (inst.sg:HasStateTag("hit") or not inst.sg:HasStateTag("busy")) then
+			if inst.rootready then
+				local stump = FindEntity(inst, TUNING.LEIF_MAXSPAWNDIST, nil, {"stump","evergreen"}, { "leif","burnt","deciduoustree" })
+				if stump ~= nil and TUNING.DSTU.PINELINGS == true then
+					inst.sg:GoToState("summon", data.target)
+				else
+					inst.sg:GoToState("snare", data.target)
+				end
+			else
+				_OldAttackEvent(inst, data)
+			end
+		else
+			_OldAttackEvent(inst, data)
+		end
+	end
+		--[[
 local events=
 {
     CommonHandlers.OnStep(),
@@ -19,7 +38,7 @@ local events=
 
     EventHandler("doattack", function(inst, data) 
             if not inst.components.health:IsDead() and (inst.sg:HasStateTag("hit") or not inst.sg:HasStateTag("busy")) then
-                    if inst.rootready == true then
+                    if inst.rootready then
 							local stump = FindEntity(inst, TUNING.LEIF_MAXSPAWNDIST, nil, {"stump","evergreen"}, { "leif","burnt","deciduoustree" })
 							if stump ~= nil and TUNING.DSTU.PINELINGS == true then
 							inst.sg:GoToState("summon", data.target)
@@ -39,7 +58,7 @@ local events=
     
     
 }
-
+]]
 local states = {
 
 	State{
@@ -54,6 +73,10 @@ local states = {
 			inst.sg.statemem.target = target
 			inst.rootready = false
 			
+        end,
+		
+        onexit = function(inst)
+			inst.components.combat:SetRange(inst.oldrange)
         end,
 
         timeline =
@@ -101,6 +124,10 @@ local states = {
 			inst.sg.statemem.target = target
 			inst.rootready = false
 			
+        end,
+		
+        onexit = function(inst)
+			inst.components.combat:SetRange(inst.oldrange)
         end,
 
         timeline =
@@ -169,10 +196,10 @@ local states = {
     },
 }
 
-for k, v in pairs(events) do
+--[[for k, v in pairs(events) do
     assert(v:is_a(EventHandler), "Non-event added in mod events table!")
     inst.events[v.name] = v
-end
+end]]
 
 for k, v in pairs(states) do
     assert(v:is_a(State), "Non-state added in mod state table!")
