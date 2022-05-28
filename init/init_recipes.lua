f33961fc5eb068e2dedcd61fb6430518c31ea8f3
AtobaAzul <atoba.azul.radical@gmail.com>
2022-05-28 20:09:52 -0300

    Fixed crash with sharkshuit, added missing recipe sorting

--- init/init_recipes.lua
+++ init/init_recipes.lua
@@ -261,7 +261,8 @@ AddRecipe2(
     TECH.NONE,
     {builder_tag = "plantkin"},
     {"CHARACTER"}
-)--the fuck
+)
+ChangeSortKey("honey_log", "livinglog", "CHARACTER", true)
 
 AddRecipe2(
     "bugzapper",
@@ -360,6 +361,7 @@ AddRecipe2(
     {builder_tag = "spiderwhisperer"},
     {"CHARACTER"}
 )
+ChangeSortKey("mutator_trapdoor", "mutator_warrior", "CHARACTER", true)
 
 AddRecipe2(
     "book_rain",
@@ -368,6 +370,7 @@ AddRecipe2(
     {builder_tag = "bookbuilder"},
     {"CHARACTER"}
 )
+ChangeSortKey("book_rain", "book_tentacles", "CHARACTER", true)
 
 AddRecipe2(
     "driftwoodfishingrod",
