e9e37a3472b8d034cdfda85c469c50c51c08446a
AtobaAzul <atoba.azul.radical@gmail.com>
2022-06-15 18:02:48 -0300

    APPEASEMENT AAAAAA IM LATE FOR SCHOOL

--- scripts/prefabs/aphid.lua
+++ scripts/prefabs/aphid.lua
@@ -177,6 +177,11 @@ local function fn()
     inst:AddComponent("knownlocations")
     inst:AddComponent("inspectable")
 
+    if TUNING.DSTU.ISLAND_ADVENTURES then
+        inst:AddComponent("appeasement")
+        inst.components.appeasement.appeasementvalue = TUNING.TOTAL_DAY_TIME
+    end
+    
     inst:ListenForEvent("attacked", OnAttacked)
 
     inst:AddComponent("eater")
