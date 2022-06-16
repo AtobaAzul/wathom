021353fb7bbc48ccb19e9bf7ad74792818a9f23f
Scrimbles <40772985+Shodance@users.noreply.github.com>
2022-06-16 11:57:06 -0400

    try this craft, updated figgy build, alarming clock gives back fuel on second naught lock hit to counter the loss

--- postinit/widgets/craftslot.lua
+++ postinit/widgets/craftslot.lua
@@ -59,63 +59,33 @@ env.AddClassPostConstruct("widgets/craftslot", function(self)
 end)
 
 env.AddClassPostConstruct("widgets/redux/craftingmenu_hud", function(self)
-	
-	local _OldOnCraftingMenuOpen = self.OnCraftingMenuOpen
 	local _OldOnUpdate = self.OnUpdate
-	local _OldOnControl = self.OnControl
-	local _OldPopulateRecipeDetailPanel = self.PopulateRecipeDetailPanel
-	local _OldRefreshCraftingHelpText = self.RefreshCraftingHelpText
 
-	function self:RefreshCraftingHelpText(...)
-		if self.uncomptip ~= nil then
-			self.uncomptip.item_tip = nil
-			self.uncomptip.skins_spinner = nil
-			self.uncomptip:HideTip()
-		end
-		
-		if self.craftingmenu ~= nil then
+	function self:OnUpdate(...)
+		if self.craftingmenu ~= nil and self.uncomptip == nil then
 			self.uncomptip = self.craftingmenu:AddChild(UncompTooltip())
 			self.uncomptip:SetPosition(-105, -210)
 			self.uncomptip:SetScale(0.35)
 		end
 		
-		--if self.last_recipe_state ~= nil and self.last_recipe_state ~= "hide" then
-			if self.craftingmenu ~= nil and 
-				self.craftingmenu.crafting_hud ~= nil and 
-				self.craftingmenu.crafting_hud:IsCraftingOpen() and 
-				self.uncomptip ~= nil and 
-				self.craftingmenu.details_root ~= nil and 
-				self.craftingmenu.details_root.data and 
-				self.craftingmenu.details_root.data.recipe ~= nil and 
-				self.craftingmenu.details_root.data.recipe.name and 
-				STRINGS.UNCOMP_TOOLTIP[string.upper(self.craftingmenu.details_root.data.recipe.name)] ~= nil then
-					self.uncomptip.item_tip = self.craftingmenu.details_root.data.recipe.name
-					self.uncomptip.skins_spinner = self.craftingmenu.details_root.skins_spinner or nil
-					self.uncomptip:ShowTip()
-			end
-		--end
-		
-		_OldRefreshCraftingHelpText(self, ...)
-	end
---[[
-	function self:OnControl(...)
-		if self.uncomptip ~= nil then
+		if self.craftingmenu ~= nil and 
+			self.craftingmenu.crafting_hud ~= nil and 
+			self.craftingmenu.crafting_hud:IsCraftingOpen() and 
+			self.uncomptip ~= nil and 
+			self.craftingmenu.details_root ~= nil and 
+			self.craftingmenu.details_root.data and 
+			self.craftingmenu.details_root.data.recipe ~= nil and 
+			self.craftingmenu.details_root.data.recipe.name and 
+			STRINGS.UNCOMP_TOOLTIP[string.upper(self.craftingmenu.details_root.data.recipe.name)] ~= nil then
+				self.uncomptip.item_tip = self.craftingmenu.details_root.data.recipe.name
+				self.uncomptip.skins_spinner = self.craftingmenu.details_root.skins_spinner or nil
+				self.uncomptip:ShowTip()
+		elseif self.uncomptip ~= nil then
 			self.uncomptip.item_tip = nil
 			self.uncomptip.skins_spinner = nil
 			self.uncomptip:HideTip()
 		end
-	
-		self.uncomptip = self:AddChild(UncompTooltip())
-		--self.uncomptip = self.root:AddChild(UncompTooltip())
-		
-		if self.last_recipe_state ~= nil and self.last_recipe_state ~= "hide" then
-			if self.uncomptip ~= nil and self.details_root ~= nil and self.details_root.data.recipe ~= nil and self.details_root.data.recipe.name and STRINGS.UNCOMP_TOOLTIP[string.upper(self.details_root.data.recipe.name)] ~= nil then
-				self.uncomptip.item_tip = self.details_root.data.recipe.name
-				self.uncomptip.skins_spinner = self.details_root.skins_spinner or nil
-				self.uncomptip:ShowTip()
-			end
-		end
-		
-		_OldOnControl(self, ...)
-	end]]
+			
+		_OldOnUpdate(self, ...)
+	end
 end)
