f2b4c5ffcdea4624b2d9eb033d5bff6d50a02dbe
TheAxeSwingingSmasher <56132394+TheAxeSwingingSmasher@users.noreply.github.com>
2022-07-14 14:17:49 -0500

    Disabled defensive mode for a bit, BQ's cross charge is closer and quicker, probably needs changing.

--- postinit/prefabs/beeguard.lua
+++ postinit/prefabs/beeguard.lua
@@ -10,22 +10,9 @@ local normalsounds =
     death = "dontstarve/creatures/together/bee_queen/beeguard/death",
 }
 
-local function MortarAttack(inst)
-	if inst.components.health and not inst.components.health:IsDead() and inst.components.combat and inst.components.combat.target then
-		inst.stabtarget = inst.components.combat.target --This is actually the fallback, we want them to attack the closest target
-		local x,y,z = inst.Transform:GetWorldPosition()
-		local combat = TheSim:FindEntities(x,y,z,12,{"_combat"},{"playerghost","bee","epic"})
-		if math.random() > 0.25 then --Occasionally make the bee just keep going after the intended player, EVEN IF they're not actually the closest option.
-			for i,ent in ipairs(combat) do
-			local distance = 1000000
-				if (ent.components.combat and ent.components.combat.target) or ent:HasTag("player") then --Target is fighting nearby (or they are a player), try for the closest option
-					if inst ~= nil and inst:IsValid() and ent:GetDistanceSqToInst(inst) < distance then
-						distance = ent:GetDistanceSqToInst(inst)
-						inst.stabtarget = ent
-					end
-				end
-			end
-		end
+local function MortarAttack(inst,target)
+	if inst.components.health and not inst.components.health:IsDead() and target then
+		inst.stabtarget = target
 		inst.sg:GoToState("flyup")
 	end
 end
