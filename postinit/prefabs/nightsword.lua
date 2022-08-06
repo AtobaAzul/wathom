0b32514875e8c7d46b70688aa86ea7edbb3974a2
AtobaAzul <atoba.azul.radical@gmail.com>
2022-08-06 00:29:52 -0300

    darksword sanity drain code improvements

--- postinit/prefabs/nightsword.lua
+++ postinit/prefabs/nightsword.lua
@@ -7,28 +7,18 @@ env.AddPrefabPostInit("nightsword", function(inst)
 		return
 	end
 
-    local function DrainSanity(inst)
-        if inst.components.sanity ~= nil and inst.age_state ~= "old" then
-            inst.components.sanity:DoDelta(-0.75*(inst:HasTag("Funny_Words_Magic_Man") and 0.66 or 1))
+    local _onattack = inst.components.weapon.onattack
+
+    local function DrainSanity(inst, attacker, target)
+        if attacker.components.sanity ~= nil and attacker.age_state ~= "old" then
+            attacker.components.sanity:DoDelta(-0.75*(attacker:HasTag("Funny_Words_Magic_Man") and 0.66 or 1))
+        end
+        if _onattack ~= nil then--mod compat/future proofing, just in case
+            _onattack(inst, attacker, target)
         end
     end
 
-    local _OnEquip = inst.components.equippable.onequipfn
-
-    inst.components.equippable.onequipfn = function(inst, owner)
-        owner:ListenForEvent("onattackother", DrainSanity)
-
-        if _OnEquip ~= nil then
-            return _OnEquip(inst, owner)
-        end
-    end
-
-    local _OnUnequip = inst.components.equippable.onunequipfn
-
-    inst.components.equippable.onunequipfn = function(inst, owner)
-        owner:RemoveEventCallback("onattackother", DrainSanity)
-        if _OnUnequip ~= nil then
-            return _OnUnequip(inst, owner)
-        end
+    if inst.components.weapon ~= nil then
+        inst.components.weapon:SetOnAttack(DrainSanity)
     end
 end)
