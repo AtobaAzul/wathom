85831b2d52a54c943a9389d77bc0bbc26e158c4b
CanisWTF <alphawolfkidgamer@gmail.com>
2022-09-11 04:44:15 +0000

    Added MiM Bio for Wathom, might as well do this now. You can read in #writing-corner

--- modclientmain.lua
+++ modclientmain.lua
@@ -69,7 +69,7 @@ STRINGS.CHARACTER_ABOUTME.wathom = "A hunter with an uncontrollable surplus of e
 STRINGS.CHARACTER_BIOS.wathom = {
  { title = "Birthday", desc = "January 20" },
  { title = "Favorite Food", desc = "Hardshell Tacos" },
- { title = "His past...", desc = "Is yet to be revealed."},
+ { title = "The Forgotten Parody", desc = "The civilization that once occupied the ruins always piqued Maxwell's curiosity. Even he on the throne didnt know all the secrets buried within the Constant. Using dusted bones and nightmare fuel, the Shadow King breathed life into a mimic of the ancient race, with the purpose to understand those who came before them. \n \nWathom never knew anything other than dank caverns and pulsating ruins - and when he didn't provide the secrets that he was born to uncover, the only thing he knew from then on was the indefinite darkness of the Abyss, banished and forgotten. At least, until the fallen moon provided climbable cracks in the walls."},
 }
 STRINGS.CHARACTER_SURVIVABILITY.wathom = "Slim"
 
