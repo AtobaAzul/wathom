ebc4a547e2cd4ed33fd35b10b38b91c1b9b9ca25
AtobaAzul <atoba.azul.radical@gmail.com>
2023-07-28 00:34:44 -0300

    ver

--- modinfo.lua
+++ modinfo.lua
@@ -19,7 +19,7 @@ Wathom completely changes the core fundamentals of gameplay; As he gains more an
 
 author = "󰀈 The Uncomp Dev Team 󰀈"
 
-version = "1.0.2.2"
+version = "1.0.2.3"
 
 forumthread = "/topic/111892-announcement-uncompromising-mode/"
 
