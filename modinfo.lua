1043dceedb19b7d0f3977b9d2f9854afdebc7f79
AtobaAzul <atoba.azul.radical@gmail.com>
2024-09-27 13:58:24 -0300

    ver

--- modinfo.lua
+++ modinfo.lua
@@ -1,6 +1,6 @@
 name = "Wathom, the Forgotten Parody"
 description = [[
-󰀔 [ Version 1.0.3.2 : "Standalone Release" ]
+󰀔 [ Version 1.0.3.3 : "Standalone Release" ]
 
 "A hunter with an uncontrollable surplus of energy, Wathom lives on after crawling out of the Abyss he was imprisoned in."
 
@@ -19,7 +19,7 @@ Wathom completely changes the core fundamentals of gameplay; As he gains more an
 
 author = "󰀈 The Uncomp Dev Team 󰀈"
 
-version = "1.0.3.2"
+version = "1.0.3.3"
 
 forumthread = "/topic/111892-announcement-uncompromising-mode/"
 
